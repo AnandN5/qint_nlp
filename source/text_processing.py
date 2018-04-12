242cfec6a8625bdd0722a75829e22b55bc923d8d
Anand_Menon <anandmenon@qburst.com>
2018-04-12 12:16:35 +0530

    Implemented combin taggers.

--- source/text_processing.py
+++ source/text_processing.py
@@ -1,33 +1,52 @@
-from nltk.corpus import stopwords
 # from nltk.corpus import wordnet
 from nltk.tokenize import word_tokenize, sent_tokenize
 import nltk
-import re
-import string
 
 
-with open('/Users/qbuser/Documents/pythonWorks/BigDataWorks/nlp_work/qint_nlp/audit1.txt', 'r') as f:
-    sentences = sent_tokenize(f.read())
-    tokens_list = []
-    sw = stopwords.words('english')
-    for index, sent in enumerate(sentences):
-        sent = re.sub('\W(?=\s|$)', '', sent)
-        tokens = [w.lower() for w in word_tokenize(sent)]
-        clean_tokens = tokens[:]
+def process_data(file):
+    with open(file, 'r') as f:
+        sentences = sent_tokenize(f.read())
+        tagged_sents = []
+        trained_token_list = []
+        for sent in sentences:
+            tagged_sents.append(nltk.pos_tag(word_tokenize(sent)))
+        train_sents, test_sents = train_test_sentences(tagged_sents)
+        trained_token_list.extend([w for sent in train_sents for w in sent])
 
-        # Removing stopwords
-        for token in tokens:
-            if token in sw or token in string.punctuation:
-                clean_tokens.remove(token)
-        tokens_list.append(nltk.pos_tag(clean_tokens))
+        # take the most likely tag for freq words and create a model
+        freq_words = nltk.FreqDist(
+            [t[0] for t in trained_token_list]).most_common()
+        cfd_freq_words = nltk.ConditionalFreqDist(trained_token_list)
+        likely_tags = dict((word, cfd_freq_words[word].max())
+                           for (word, _) in freq_words)
 
-    for index, item in enumerate(tokens_list):
-        print('{}. \n'.format(index))
-        grammar = r"""
-                    # chunk determiner/possessive, adjectives and noun
-                    audit_number: {<DT|PP\$>?<JJ>*<NN>}
-                    non-confrmance: {<VBD>+}                # chunk sequences of proper nouns
-                """
+        # default tagger
+        default_tagger = nltk.DefaultTagger('NIL')
 
-        cp = nltk.RegexpParser(grammar)
-        print(type(cp.parse(item)))
+        # Unigram Tagger
+        u_tagger = nltk.UnigramTagger(
+            model=likely_tags, backoff=default_tagger)
+
+        # Bigram_tagger
+        b_tagger = nltk.BigramTagger(model=likely_tags, backoff=u_tagger)
+        print('Evaluation: ', b_tagger.evaluate(test_sents))
+
+        tagged_tests = b_tagger.tag(
+            [w for sent in sentences for w in word_tokenize(sent)])
+        print(tagged_tests[:50])
+
+
+def train_test_sentences(tagged_sentences):
+    size = int(len(tagged_sentences) * 0.9)
+    train_sents = tagged_sentences[:size]
+    test_sents = tagged_sentences[size:]
+    return train_sents, test_sents
+
+
+def main():
+    filename = '/Users/qbuser/Documents/pythonWorks/BigDataWorks/qint_nlp/source/audit1.txt'
+    process_data(file=filename)
+
+
+if __name__ == "__main__":
+    main()
